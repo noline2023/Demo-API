0ca1cb0b1ec1b6077f0da1fdac2c464e90ace9c2
shonhazan123 <75479393+shonhazan123@users.noreply.github.com>
2022-12-17 17:20:53 +0200

    last update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const options ={
         },
         servers:[
             {
-                url:"http://localhost:5000"
+                url:"34.224.91.97:5000"
             }
         ]
     },  
